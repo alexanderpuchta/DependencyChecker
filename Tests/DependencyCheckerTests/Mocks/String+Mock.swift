8a5af86ccf00f1498002b2c6de9897e58be1725e
Alexander Puchta <hi@alexanderpuchta.de>
2024-02-14 22:01:04 +0100

    [ADD] git tag helper tests

--- Tests/DependencyCheckerTests/Mocks/String+Mock.swift
+++ Tests/DependencyCheckerTests/Mocks/String+Mock.swift
@@ -6,8 +6,21 @@ import Foundation
 
 extension String {
     
+    // MARK: - Git
+    
+    static func gitLsMockWithVersion(_ version: String) -> String {
+        """
+            23a8863b7f0dacfb4a289de2db1200b88c02aa5f\t    refs/tags/0.0.1\n
+            23a8863b7f0dacfb4a289de2db1200b88c02aa5f\t    refs/tags/\(version)
+        """
+    }
+    
+    // MARK: - Packages
+    
     static let packageNameMock = "Dependency-Checker"
     static let urlStringMock = "https://github.com/alexanderpuchta/DependencyChecker"
+    
+    // MARK: - Versions
     
     static let versionDefaultMock = "1.0.0"
     static let versionMajorMock = "2.0.0"
